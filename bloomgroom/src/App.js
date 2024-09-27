519f2bed06e51f207fda7a059778cc2c34a606c7
조하은 <jhe93@naver.com>
2024-09-28 03:24:05 +0900

    Revert "Merge branch 'dev' of https://github.com/Groom-Team11/Front into feat4-set-longterm-goals"
    This reverts commit b554f8f410eea6d766014bcbbf9e631316b0bbb9, reversing
    changes made to c51d0064a64217df36dc4c3ad6d2adca12764bc5.

--- bloomgroom/src/App.js
+++ bloomgroom/src/App.js
@@ -1,20 +1,12 @@
 
 import './App.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import StartRoutes from './routes/startRoutes';
-import PageRoutes from './routes/pageRoutes';
+import LoginPage from './login';
 import GoalPage from './GoalPage';
 
 function App() {
   return (
-    // <Router>
-    //   <StartRoutes />
-    //   <PageRoutes />
-    // </Router>
-    <GoalPage/>
-
+    <div>
+    </div>
   );
 }
 

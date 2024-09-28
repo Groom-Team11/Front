e9f761d04db6c0ba243eb8bb040e394b940b14d0
조하은 <jhe93@naver.com>
2024-09-28 20:32:22 +0900

    test

--- bloomgroom/src/App.js
+++ bloomgroom/src/App.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import StartRoutes from './routes/startRoutes';
 import PageRoutes from './routes/pageRoutes';
-
+import Flower from '../common/flower';
+import GoalPage from '../GoalPage/index';
+import LoginPage from '../login/index';
 function App() {
   return (
     <Router>
